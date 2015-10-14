f15a142d0f7be8430a0c06a4f33849f8eeef2938
Luke Smith <luke.smith112358@gmail.com>
2015-10-14 08:43:18 -0600

    Working on memory. Rename build scripts

--- src/kernel/arch/i686/mem.c
+++ src/kernel/arch/i686/mem.c
@@ -7,5 +7,25 @@ uintptr_t placement_address = (uintptr_t)&end;
 
 uintptr_t _kmalloc(size_t size, uint8_t align, uintptr_t *phys)
 {
+	if(align == 1 && (placement_address & 0xFFFFF000))
+	{
+		placement_address &= 0xFFFFF000
+		placement_address += 0x00001000
+	}	
+	if(phys)
+	{
+		phys = placement_address;
+	}
+	uintptr_t addr = placement_address;
+	placement_address += size;
+	return addr;
+}
+
+uintptr_t kmalloc(size_t size) { return _kmalloc(size, 0, NULL); }
+uintptr_t kmalloc_a(size_t size) { return _kmalloc(size, 1, NULL); }
+uintptr_t kmalloc_p(size_t size, uintptr_t *phys) { return _kmalloc(size, 0, phys); }
+uintptr_t kmalloc_ap(size_t size, uintptr_t *phys) { return _kmalloc(size, 1, phys); }
+
+void kfree(void *p) {
 	
 }
