f1819ca74ecdeefc041c61b2a96731a1b077e15c
Luke Smith <luke.smith112358@gmail.com>
2015-12-18 11:17:55 -0700

    Updated dir structure and build system. x86_64 started

---
+++ src/kernel/arch/x86_64/misc/utils.c
@@ -0,0 +1,55 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <misc/utils.h>
+
+void outb(uint16_t port, uint8_t val)
+{
+	__asm__ __volatile__ ("outb %b0, %w1" : : "a"(val), "Nd"(port));
+}
+
+void outw(uint16_t port, uint16_t val)
+{
+	__asm__ __volatile__ ("outw %w0, %w1" : : "a"(val), "Nd"(port));
+}
+
+void outl(uint16_t port, uint32_t val)
+{
+	__asm__ __volatile__ ("outl %0, %w1" : : "a"(val), "Nd"(port));
+}
+
+uint8_t inb(uint16_t port)
+{
+	uint8_t ret;
+	__asm__ __volatile__ ("inb %w1, %b0" : "=a" (ret) : "dN" (port));
+	return ret;
+}
+
+uint16_t inw(uint16_t port)
+{
+	uint16_t ret;
+	__asm__ __volatile__ ("inw %w1, %w0" : "=a" (ret) : "dN" (port));
+	return ret;
+}
+
+uint32_t inl(uint16_t port)
+{
+	uint32_t ret;
+	__asm__ __volatile__ ("inl %w1, %0" : "=a" (ret) : "dN" (port));
+	return ret;
+}
+
+void io_wait()
+{
+	__asm__ __volatile__ ( "outb %%al, $0x80" : : "a"(0) );
+}
+
+uint8_t one_count(uint32_t value)
+{
+	int cnt = 0;
+	while(value)
+	{
+		cnt += value & 0x1;
+		value >>= 1;
+	}
+	return cnt;
+}
--- src/kernel/module.mk
+++ src/kernel/module.mk
@@ -1,3 +1,2 @@
-KSRC += ../src/kernel/kernel.c 							\
-$(shell find ../src/kernel/arch/$(ARCH)/ -name '*.c')	\
+KSRC += $(shell find ../src/kernel/arch/$(ARCH)/ -name '*.c') \
 $(shell find ../src/kernel/arch/$(ARCH)/ ! -name 'crt*.S' -name '*.S')
